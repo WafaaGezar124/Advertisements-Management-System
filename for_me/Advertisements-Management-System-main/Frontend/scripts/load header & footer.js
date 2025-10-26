57b59b45f555bd12e7e5e30780d6f1a264a16e62
w3schools-test <test@w3schools.com>
2025-10-26 15:58:43 +0300

    Dashboard update

--- for_me/Advertisements-Management-System-main/Frontend/scripts/load header & footer.js
+++
@@ -1,7 +0,0 @@
-fetch("../components/header.html")
-    .then(r => r.text())
-    .then(d => document.querySelector("header").innerHTML = d);
-
-fetch("../components/footer.html")
-    .then(r => r.text())
-    .then(d => document.querySelector("footer").innerHTML = d);
